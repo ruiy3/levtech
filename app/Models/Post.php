ae14586c5298c08ecf26c4903bf3816a76c146da
ruiy3 <rui.y.312312@outlook.jp>
2023-07-14 10:00:04 +0900

    add . ブログ投稿作成画面の保存処理実装

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,10 +14,16 @@ class Post extends Model
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }*/
     
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
     public function getPaginateByLimit(int $limit_count = 10)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
 }
 
