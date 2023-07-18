3d960810c51870a7a61c70c14802e522635f9bc6
ruiy3 <rui.y.312312@outlook.jp>
2023-07-18 12:13:41 +0900

    add .9-1と9-2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,5 +19,11 @@ class Post extends Model
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
+    //「1対多」の関係なので単数系に
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
 
